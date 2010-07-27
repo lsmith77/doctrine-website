5570e5cffcf7c4faaf5679ce37011fb4f84daf6d
root <root@doctrine.(none)>
2010-07-27 11:36:46 -0400

    i

--- lib/form/sfGuardUserAdminForm.class.php
+++ lib/form/sfGuardUserAdminForm.class.php
@@ -2,11 +2,4 @@
 
 class sfGuardUserAdminForm extends BasesfGuardUserAdminForm
 {
-  public function configure()
-  {
-    parent::configure();
-
-    $choices = sfGuardUser::getSvnAccessItems();
-    $this->widgetSchema['svn_access'] = new sfWidgetFormSelectMany(array('multiple' => true, 'choices' => $choices));
-  }
 }
